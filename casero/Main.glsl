9010a82596167260e436af83af4c79b3550f5449
davidgfb <davidgracia99@gmail.com>
2023-08-13 13:35:20 +0200

    Update Main.glsl

--- casero/Main.glsl
+++ casero/Main.glsl
@@ -4,35 +4,53 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
 
     // Time varying pixel color
     //vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
-
-    int[] igual = int[](0xF, 0, 0, 0xF); //0xF00F;
+       
+    vec3 col = vec3(0);
     
-    //int _0xF = 
-    
+    /*int[] igual = int[](0xF, 0, 0, 0xF); //0xF00F;
+      
     for (int posFila = 0; posFila < 4; posFila++) {
         int nFila = igual[posFila]; //15
         
         //equivalente en iterativo a %2, %2%2, %2%2%2...
         int _4 = nFila % 2, _3 = _4 % 2, _2 = _3 % 2, _1 = _2 % 2;
         
-        int[] nBin = int[](_1, _2, _3, _4);
+        int[] nBin = int[](_1, _2, _3, _4); 
         
-        /*
-        for (int nCol = 0; nCol < 4; nCol++) {
-            fila[nCol];
-        }
-        */
+        //cjto
+        int bin = 1000 * _1 + 100 * _2 + 10 * _3 + _4;
+        
+        //
+        
+        
+        //for (int nCol = 0; nCol < 4; nCol++) {
+        //    fila[nCol];
+        //}
+        
     }
+    */
+    
+    //int[] aBin = int[](_1, _2, _3, _4);     
+    
+    /*
+    //DEMO
+    int hex = 0xF, _4 = hex % 2, _3 = _4 % 2, _2 = _3 % 2, _1 = _2 % 2, 
+        bin = 1000 * _1 + 100 * _2 + 10 * _3 + _4;
+    
+    if (bin == hex) col = vec3(1); //NO es igual hex q bin!!
+    */
     
     int xPix = int(fragCoord.x), yPix = int(fragCoord.y);
     
-    vec3 col = vec3(0);
     
+    
+    /*
     //par impar
     //esPar
     if (xPix % 2 == 0) {
         col = vec3(1);
     }
+    */
 
     // Output to screen. Hay que normalizarlo ie dividirlo entre iRes
     fragColor = vec4(col, 1.0);
