127b22db1f5675d6b8b214d22e9fa5d2927e741e
davidgfb <davidgracia99@gmail.com>
2023-08-13 17:13:23 +0200

    Update temp.glsl

--- casero/temp.glsl
+++ casero/temp.glsl
@@ -1,34 +1,11 @@
-/*
-void _4x4() {
-    vec4(0);
-    vec4(0);
-    vec4(0);
-    vec4(0);
-}
-*/
-
-
-//hex -> mat4
-//mat4 a;
-
-
-int[4] hex_A_Bin(int hex) { //int_A_Ar_Flt
+int[4] hex_A_Bin(int hex) { //int_A_Ar_Int
     int _3 = hex % 2, _2 = hex / 2 % 2, _1 = hex / 4 % 2, _0 = hex / 8 % 2;     
     int[] ar_Int = int[](_0, _1, _2, _3);
     
-    /*
-    const int n_Elems = ar_Int.length();
-    float[n_Elems] ar_Flt;
-    
-    for (int i = 0; i < n_Elems; i++) {
-        ar_Flt[i] = float(ar_Int[i]);
-    }
-    */
-    
-    return ar_Int; //ar_Flt;
+    return ar_Int; 
 }
 
-mat4 set_Fila(mat4 m, int n_Fila, int[4] ar_Int) { //SOLO AQUI float
+mat4 set_Fila(mat4 m, int n_Fila, int[4] ar_Int) { 
     for (int i = 0; i < 4; i++) {
         m[n_Fila][i] = float(ar_Int[i]);
     }
@@ -46,7 +23,6 @@ void mainImage( out vec4 fragColor, vec2 fragCoord ) {
     
     //int -> vec4
     //int n = 0xF, n1 = 9; //0xF99F
-    
     //int n = 1; //0xF00F; //61455
     
     //15
@@ -57,8 +33,6 @@ void mainImage( out vec4 fragColor, vec2 fragCoord ) {
     */
     //vec4 v = vec4(0);
 
-     //n,n1,n1,n
-
     int[] ar_Int = hex_A_Bin(0xF), ar_Int1 = hex_A_Bin(9);
     
     mat4 m;
@@ -67,34 +41,10 @@ void mainImage( out vec4 fragColor, vec2 fragCoord ) {
     m = set_Fila(m, 1, ar_Int1);
     m = set_Fila(m, 2, ar_Int1);
     m = set_Fila(m, 3, ar_Int);
-
-    
-    
-
-    /*
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            m[i][j] = 0.0;
-        }
-    }
-    */
-    
-    /*
-    mat4 m = mat4(1,1,1,1, //0xF, 15, 0-15
-        1,0,0,1, //9
-        1,0,0,1,
-        1,1,1,1);
-    */
     
     col = vec3(m[xPix % 4][yPix % 4]);
-
-    
-    
-    
-    
-    //if ([xPix % 4]) col = vec3(1);
-    
-    
+ 
+    //if ([xPix % 4]) col = vec3(1);  
     //if (xPix % 2 == 0) col = vec3(1); //&& yPix % 2 == 0) col = vec3(1);
 
     // Output to screen
