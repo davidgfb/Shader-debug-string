341618188abff074ca2a1a5d557c50e188469c41
davidgfb <davidgracia99@gmail.com>
2023-08-13 14:27:59 +0200

    Update Dbug.glsl

--- casero/Dbug.glsl
+++ casero/Dbug.glsl
@@ -292,9 +292,9 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     chr += drawChar( CH_X, charPos, charSize, uv); pos += pow(20.0, -1.0); charPos = vec2(pos, 0.8);
     chr += drawChar( CH_EQUL, charPos, charSize, uv); pos += pow(6.0, -1.0); charPos = vec2(pos, 0.8);
 
-    
-    chr += drawIntCarriage( hex, charPos, charSize, uv, 4); pos += 0.1; charPos = vec2(pos, 0.8);
-    chr += drawChar( CH_D, charPos, charSize, uv); pos += 0.1; charPos = vec2(pos, 0.8);
+    chr += drawIntCarriage( hex, charPos, charSize, uv, 4); pos += pow(20.0, -1.0); charPos = vec2(pos, 0.8);
+
+    chr += drawChar( CH_D, charPos, charSize, uv); pos += pow(20.0, -1.0); charPos = vec2(pos, 0.8);
     chr += drawChar( CH_EQUL, charPos, charSize, uv);
     
     
